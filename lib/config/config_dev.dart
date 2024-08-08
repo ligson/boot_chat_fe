c01f80c6a40214671c99c458cd2099b8e77c151a
liyin <liyin@yonyou.com>
2024-08-08 19:28:27 +0800

    修改流式接口认证问题

--- lib/config/config_dev.dart
+++ lib/config/config_dev.dart
@@ -1,6 +1,6 @@
 class Config {
-  static const String baseUrl = 'https://ydf-i.yonyougov.top';
+//static const String baseUrl = 'https://ydf-i.yonyougov.top';
   //static const String wsBaseUrl = 'wss://demo.xassn.xyz/xassn-be';
-//static const String baseUrl = 'http://127.0.0.1:8080';
+  static const String baseUrl = 'http://127.0.0.1:8080/boot-chat';
 //static const String wsBaseUrl = 'ws://127.0.0.1:8080/xassn-be';
 }
